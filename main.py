62d3c4c1ca26655ca40651d7d89fe06a09987863
Denishka <denis.sabirzianow@yandex.ru>
2023-12-11 16:52:56 +0400

    Regions are displayed correctly when selecting multiple regions

--- main.py
+++ main.py
@@ -1,26 +1,22 @@
 import asyncio
 from contextlib import suppress
-
-import psycopg2
+from typing import Optional
 from aiogram import Dispatcher, Bot
 from aiogram import F, Router, types
 from aiogram import exceptions
 from aiogram.enums.chat_type import ChatType
 from aiogram.exceptions import TelegramBadRequest
 from aiogram.filters import Command
+from aiogram.filters.callback_data import CallbackData
 from aiogram.filters.state import State, StatesGroup
 from aiogram.fsm.context import FSMContext
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton, InlineKeyboardMarkup
-from aiogram.types import ReplyKeyboardRemove, \
-    ReplyKeyboardMarkup, KeyboardButton, \
-    InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from aiogram.utils.keyboard import InlineKeyboardBuilder
-from typing import Optional
-from aiogram.filters.callback_data import CallbackData
 
 from config_reader import config
 from database_manager import init_database, get_connection_to_database, get_all_regions, get_chat_ids, \
-    delete_user_from_database, add_user_to_region, insert_user_to_database, get_user_by_username_from_database, get_user_by_id_from_database
+    delete_user_from_database, add_user_to_region, insert_user_to_database, get_user_by_username_from_database, \
+    get_user_by_id_from_database
 
 bot = Bot(token=config.bot_token.get_secret_value())
 
@@ -46,10 +42,10 @@ class RegionCallbackFactory(CallbackData, prefix="region"):
     region: Optional[int] = None
 
 
-async def update_region_text_fab(message: types.Message, new_region: str, regions):
+async def update_region_text_fab(message: types.Message, new_regions: list, regions):
     with suppress(TelegramBadRequest):
         await message.edit_text(
-            f"В какой регион вы хотите добавить пользователя с именем {forwarded_users[319186657]['username']} и ID {forwarded_users[319186657]['user_id']} ? Выбранный регион:  {new_region}",
+            f"В какие регионы вы хотите добавить пользователя с именем {forwarded_users[319186657]['username']} и ID {forwarded_users[319186657]['user_id']} ? Выбранные регионы:  {', '.join(new_regions)}",
             reply_markup=get_keyboard_fab(regions)
         )
 
@@ -60,13 +56,17 @@ async def callbacks_region_select(
         callback_data: RegionCallbackFactory
 ):
     user_info = forwarded_users.get(callback.from_user.id, {})
-    # Текущий выбранный регион
-    user_info['region'] = callback_data.region
+    # Если регион еще не выбран, инициализируем список
+    if 'regions' not in user_info:
+        user_info['regions'] = []
+    # Добавляем выбранный регион в список
+    user_info['regions'].append(callback_data.region)
     forwarded_users[callback.from_user.id] = user_info
-    region_name = get_region_name_by_id(callback_data.region)
+    region_names = [get_region_name_by_id(region) for region in user_info['regions']]
     regions = get_all_regions()
-    await update_region_text_fab(callback.message, region_name, regions)
+    await update_region_text_fab(callback.message, region_names, regions)
     await callback.answer()
+
 
 
 @dp.callback_query(RegionCallbackFactory.filter(F.action == "finish"))
@@ -74,14 +74,16 @@ async def callbacks_region_finish(
         callback: types.CallbackQuery,
         callback_data: RegionCallbackFactory
 ):
-    # Используем user_id, username и выбранный регион из временного хранилища
+    # Используем user_id, username и выбранные регионы из временного хранилища
     user_info = forwarded_users.get(callback.message.chat.id)
-    if user_info is not None and 'region' in user_info:
-        await add_user_to_region(user_info['user_id'], user_info['username'], user_info['region'])
+    if user_info is not None and 'regions' in user_info:
+        for region in user_info['regions']:
+            await add_user_to_region(user_info['user_id'], user_info['username'], region)
+        region_names = [get_region_name_by_id(region) for region in user_info['regions']]
         await callback.message.edit_text(
-            f"Выбор региона завершен. Выбранный регион: {get_region_name_by_id(user_info['region'])}")
+            f"Выбор регионов завершен. Выбранные регионы: {', '.join(region_names)}")
     else:
-        await callback.message.edit_text("Выбор региона завершен.")
+        await callback.message.edit_text("Выбор регионов завершен.")
     await callback.answer()
 
 
@@ -110,7 +112,7 @@ async def forward_message(message: types.Message):
         keyboard = get_keyboard_fab(regions)
 
         await message.answer(
-            f"В какой регион вы хотите добавить пользователя с именем {username} и ID {user_id} ? Выбранный регион: ",
+            f"В какие регионы вы хотите добавить пользователя с именем {username} и ID {user_id} ? Выбранные регионы: ",
             reply_markup=keyboard
         )
 
