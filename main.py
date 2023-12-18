a59d65884d97a1155f819b16c04ec2b5238b867a
Denishka <denis.sabirzianow@yandex.ru>
2023-12-18 23:30:59 +0400

    fix bugs

--- main.py
+++ main.py
@@ -52,6 +52,7 @@ def get_invite_links_for_user(user_id):
 
     return invite_links
 
+
 @dp.callback_query(lambda c: c.data == 'get_links')
 async def process_callback(callback_query: types.CallbackQuery):
     user_id = callback_query.from_user.id
@@ -80,7 +81,12 @@ async def update_region_text_fab(message: types.Message, new_regions: list, regi
             f"В какие регионы вы хотите добавить пользователя с именем {forwarded_users[319186657]['username']} и ID {forwarded_users[319186657]['user_id']} ? Выбранные регионы:  {', '.join(new_regions)}",
             reply_markup=get_keyboard_fab(regions)
         )
-
+async def update_region_text_fab_2(message: types.Message, new_region: str, regions):
+    with suppress(TelegramBadRequest):
+        await message.edit_text(
+            f"В какие регионы вы хотите добавить ссылки-приглашения? Выбранный регион: {new_region}",
+            reply_markup=get_keyboard_fab_2(regions)
+        )
 
 @dp.callback_query(RegionCallbackFactory.filter(F.action == "select"))
 async def callbacks_region_select(
@@ -97,6 +103,21 @@ async def callbacks_region_select(
     region_names = [get_region_name_by_id(region) for region in user_info['regions']]
     regions = get_all_regions()
     await update_region_text_fab(callback.message, region_names, regions)
+    await callback.answer()
+
+
+@dp.callback_query(RegionCallbackFactory.filter(F.action == "select_one_region"))
+async def callbacks_region_select(
+        callback: types.CallbackQuery,
+        callback_data: RegionCallbackFactory
+):
+    user_info = forwarded_users.get(callback.from_user.id, {})
+    # Устанавливаем выбранный регион
+    user_info['region'] = callback_data.region
+    forwarded_users[callback.from_user.id] = user_info
+    region_name = get_region_name_by_id(user_info['region'])
+    regions = get_all_regions()
+    await update_region_text_fab_2(callback.message, region_name, regions)
     await callback.answer()
 
 
@@ -130,6 +151,19 @@ def get_keyboard_fab(regions):
     builder.adjust(2)
     return builder.as_markup()
 
+def get_keyboard_fab_2(regions):
+    builder = InlineKeyboardBuilder()
+    for region in regions:
+        builder.button(
+            text=region[1], callback_data=RegionCallbackFactory(action="select_one_region", region=region[0])
+        )
+    builder.button(
+        text="Подтвердить", callback_data=RegionCallbackFactory(action="finish")
+    )
+    # Выравниваем кнопки по 3 в ряд
+    builder.adjust(2)
+    return builder.as_markup()
+
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
     # if message.chat.type != 'private':
@@ -141,6 +175,7 @@ async def cmd_start(message: types.Message):
         [
             types.KeyboardButton(text="Удалить пользователя"),
             types.KeyboardButton(text="Получить ссылки"),
+            types.KeyboardButton(text="Добавить ссылки"),
         ],
     ]
     keyboard = types.ReplyKeyboardMarkup(
@@ -152,14 +187,25 @@ async def cmd_start(message: types.Message):
     await message.reply("Выберите действие", reply_markup=keyboard)
 
 
+@dp.message(F.text.lower() == "добавить ссылки")
+async def with_puree(message: types.Message):
+    regions = get_all_regions()
+    keyboard = get_keyboard_fab_2(regions)
+    await message.answer(
+        f"В какие регионы вы хотите добавить ссылки-приглашения? Выбранный регион: ",
+        reply_markup=keyboard
+    )
+
+
 @dp.message(F.text.lower() == "получить ссылки")
 async def with_puree(message: types.Message, state: FSMContext):
-        result = get_invite_links_for_user(message.from_user.id)
-        if result:
-            result_text = "\n".join(result)
-            await bot.send_message(message.from_user.id, result_text)
-        else:
-            await bot.send_message(message.from_user.id, "Извините, ссылок для вас не найдено.")
+    result = get_invite_links_for_user(message.from_user.id)
+    if result:
+        result_text = "\n".join(result)
+        await bot.send_message(message.from_user.id, result_text)
+    else:
+        await bot.send_message(message.from_user.id, "Извините, ссылок для вас не найдено.")
+
 
 @dp.message(F.text.lower() == "удалить пользователя")
 async def with_puree(message: types.Message, state: FSMContext):
@@ -172,8 +218,6 @@ async def with_puree(message: types.Message, state: FSMContext):
         await message.reply("Напишите username пользователя, которого хотите удалить:")
 
 
-
-
 @dp.chat_member()
 async def new_chat_member(update: types.ChatMemberUpdated):
     if update.new_chat_member.status == 'member':
@@ -182,8 +226,6 @@ async def new_chat_member(update: types.ChatMemberUpdated):
         if not users:
             insert_user_to_database(user)
         await bot.send_message(update.chat.id, f'Привет, {user.full_name}, как дела? Ваш ID: {user.id}')
-
-
 
 
 async def delete_user_from_chats(user, username, message):
@@ -249,6 +291,7 @@ async def process_dont_like_write_bots(message: types.Message, state: FSMContext
     await cmd_start(message)
     await state.clear()
 
+
 @dp.message()
 async def forward_message(message: types.Message):
     if message.forward_from:
@@ -265,6 +308,7 @@ async def forward_message(message: types.Message):
         )
     else:
         return
+
 
 async def main():
     await dp.start_polling(bot)
